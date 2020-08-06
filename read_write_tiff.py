d19c593e62d35a4a5edf3b901fcc1d36dc41aebd
wasimusu <wasim.khan@aggiemail.usu.edu>
2020-08-06 13:44:07 -0600

    can write masked TIFFs

--- read_write_tiff.py
+++ read_write_tiff.py
@@ -3,7 +3,24 @@ import sys
 import ogr, osr
 import os
 import numpy as np
+import cv2
 
+
+def mask_image(image, nodataval):
+    # 0 for full transparency, 255 for fully opaque
+    alpha = 0
+    for i in range(3):
+        layer = image[:, :, i]
+        if i == 0:
+            alpha = layer.copy()
+            alpha[layer == nodataval] = 0
+            alpha[layer != nodataval] = 255
+        layer[layer == nodataval] = 255
+
+    row, col, _ = image.shape
+    row, col, _ = image.shape
+    image = np.dstack((image, alpha))
+    return image
 
 def array2raster(newRasterfn, array):
     cols = array.shape[1]
@@ -11,22 +28,18 @@ def array2raster(newRasterfn, array):
 
     driver = gdal.GetDriverByName('GTiff')
     gdal.SetConfigOption('GDAL_TIFF_INTERNAL_MASK', 'YES')
-    ops = ["INTERLEAVE=BAND", "TILED=YES", "ALPHA=YES"]
+    ops = ["COMPRESS=LZW", "INTERLEAVE=BAND", "TILED=YES", "ALPHA=YES"]
     outRaster = driver.Create(newRasterfn, cols, rows, array.shape[2], gdal.GDT_Byte, ops)
+    print("Output band size ", outRaster.RasterCount)
     for band in range(outRaster.RasterCount):
         outband = outRaster.GetRasterBand(band + 1)
         outband.WriteArray(array[:, :, band])
-        outRasterSRS = osr.SpatialReference()
-        outRasterSRS.ImportFromEPSG(4326)
-        outRaster.SetProjection(outRasterSRS.ExportToWkt())
-    outband.FlushCache()
+        outband.FlushCache()
     del array
 
 
 # Open dataset
 dataset = gdal.Open('test0.TIF', gdal.GA_Update)
-
-print(dataset.GetMetadata())
 
 # Get Raster band
 gdal.UseExceptions()
@@ -41,20 +54,20 @@ try:
             continue
 
         nodataval = src_band.GetNoDataValue()
+        nodataval = 0 if nodataval is None else nodataval
         stats = src_band.GetStatistics(True, True)
         print("Min \t{} Max\t{} Mean \t{} Stddev \t{}".format(*stats))
-        # print("Nodata ", nodataval)
-        # print("Scale ", src_band.GetScale())
-        # print("Unit type ", src_band.GetUnitType())
-        # print("ColorTable ", src_band.GetColorTable())
-
+        print("Nodata ", nodataval)
 
         src_band = src_band.ReadAsArray()
-        src_band[src_band == nodataval] = 255
-
-        if (band == 0):
-            alpha = np.ones_like(src_band)
-            output_image.append(alpha)
+        # src_band[src_band == nodataval] = 255
+        #
+        # if (band == 0):
+        #     ref_band = dataset.GetRasterBand(1).ReadAsArray()
+        #     alpha = np.zeros_like(ref_band)
+        #     alpha[ref_band == nodataval] = 255
+        #     alpha[ref_band != nodataval] = 0
+        #     output_image.append(alpha)
 
         output_image.append(src_band)
         del src_band
@@ -64,7 +77,7 @@ except RuntimeError as e:
 
 # Close dataset
 dataset = None
-
 # Write the output_image
 output_image = np.stack(output_image, axis=2)
-array2raster("test00.TIF", output_image)
+output_image = mask_image(output_image, nodataval)
+array2raster("tested.TIF", output_image)
